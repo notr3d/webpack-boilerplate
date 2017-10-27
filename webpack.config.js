d8e3fcf21b98391cc0cd9119f1031392bba080e8
Alexander Nekrasov <an@kodix.ru>
2017-10-27 15:12:39 +0300

    webpack plugins

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+module.exports = {
+    entry: ["./src/js/index.js", "./src/_scss/main.scss"],
+    output: {
+        filename: "js/index.js",
+        path: path.join(__dirname, "./build/")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{ loader: "html-loader", options: { minimize: true } }]
+            },
+            {
+                test: /\.(png|jpe?g)/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            name: "./img/[name].[ext]",
+                            limit: 10000
+                        }
+                    },
+                    {
+                        loader: "img-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: { minimize: true }
+                        },
+                        { loader: "postcss-loader" },
+                        { loader: "sass-loader" }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new ExtractTextPlugin({
+            filename: "css/main.css"
+        })
+    ]
+};
